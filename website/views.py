55354cb7bd00016b4a7817a104cc290f228af696
minhquang5014 <minhquang5014@gmail.com>
2025-02-19 23:40:44 +0700

    push the website, adding robot courses

--- website/views.py
+++ website/views.py
@@ -7,6 +7,34 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
-@views.route('/courses', methods=['GET', 'POST'])
+@views.route('/scratch', methods=['GET', 'POST'])
 def courses():
-    return render_template("courses.html")
+    return render_template("scratch.html")
+
+@views.route('/python', methods=['GET', 'POST'])
+def python():
+    return render_template("python.html")
+
+@views.route('/game-maker', methods=['GET', 'POST'])
+def game_maker():
+    return render_template("game-maker.html")
+
+@views.route('/app-inventor', methods=['GET', 'POST'])
+def app_inventor():
+    return render_template("app-inventor.html")
+
+@views.route('/uaro', methods=['GET', 'POST'])
+def uaro():
+    return render_template("uaro.html")
+
+@views.route('/arduino', methods=['GET', 'POST'])
+def arduino():
+    return render_template("arduino.html")
+
+@views.route('/raspberry', methods=['GET', 'POST'])
+def raspberry():
+    return render_template("raspberry.html")
+
+@views.route('/rover-stem', methods=['GET', 'POST'])
+def rover_stem():
+    return render_template("rover-stem.html")
